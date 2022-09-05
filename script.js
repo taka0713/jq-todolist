a704dbbfb5b71d1e81646184617ab60c2c414d81
taka0713 <tyunisu2018@gmail.com>
2022-09-05 15:10:02 +0900

    message

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-$(function () {});
+$(function () {
+  let count = 0;
+  $("#register").on("click", function () {
+    const task = $("#task").val();
+    let delete_btn = '<button class="delete">削除</button>';
+    $("#task_list").append(
+      "<li data-count=" + count + ">" + task + delete_btn + "</li>"
+    );
+    count++;
+  });
+
+  //   $(document).on("click", ".delete", function () {
+  //     console.log("aaa");
+  //     console.log($(this).parent());
+  //   });
+  $(".delete").on("click", function () {
+    console.log("unko");
+  });
+});
