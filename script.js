6e79b3979beffc2c9cb6e8f3c1888baf94fb7049
taka0713 <tyunisu2018@gmail.com>
2022-09-08 10:04:09 +0900

    message

--- script.js
+++ script.js
@@ -2,18 +2,32 @@ $(function () {
   let count = 0;
   $("#register").on("click", function () {
     const task = $("#task").val();
-    let delete_btn = '<button class="delete">削除</button>';
-    $("#task_list").append(
-      "<li data-count=" + count + ">" + task + delete_btn + "</li>"
-    );
-    count++;
+    let delete_btn =
+      '<button class="delete">削除</button> <button class="completion_button">完了</button>';
+
+    if ($("#task").val() === "") {
+      alert("登録できません。");
+    } else {
+      $("#task_list").append(
+        "<li data-count=" + count + ">" + task + delete_btn + "</li>"
+      );
+      count++;
+      $("#task").val("");
+    }
   });
 
-  //   $(document).on("click", ".delete", function () {
-  //     console.log("aaa");
-  //     console.log($(this).parent());
-  //   });
-  $(".delete").on("click", function () {
-    console.log("unko");
+  $(document).on("click", ".delete", function () {
+    $(this).parent().remove();
+  });
+  // $(".delete").on("click", function () {
+  //   console.log("hoge");
+  // });
+  $(document).on("click", ".completion_button", function () {
+    console.log($(".completion_button").parent());
+    // let delete_btn =
+    //   '<button class="delete">削除</button> <button class="completion_button">完了</button>';
+    // $("#completion_list ").append(
+    //   "<li data-count=" + count + ">" + delete_btn + "</li>"
+    // );
   });
 });
