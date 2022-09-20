4ae105cb236fb1d67e10f97e90b1e0d5b2cfa638
taka0713 <tyunisu2018@gmail.com>
2022-09-20 16:16:01 +0900

    message

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 $(function () {
-  let count = 0;
-
   $("#register").on("click", function () {
     const task = $("#task").val();
     let delete_btn =
       '<button class="delete">削除</button> <button class="completion_button">完了</button>';
     let do_name = $("[name=example]").val();
-    if ($("#task").val() === "") {
+    if (task.match(/^( |　)*$/)) {
       alert("登録できません。");
     } else {
       $("#task_list").append(
-        "<li data-count=" + count + ">" + task + delete_btn + do_name + "</li>"
+        "<li>" + "[" + do_name + "]" + task + delete_btn + "</li>"
       );
-      count++;
+
       $("#task").val("");
     }
   });
@@ -25,9 +23,7 @@ $(function () {
     let back_btn =
       '<button class="delete">削除</button> <button class="return_button">戻す</button>';
     $("#completion_list").append(
-      "<li data-count=" +
-        count +
-        ">" +
+      "<li>" +
         $(this).parent().text().split("\n")[0].replace("削除 完了", "") +
         back_btn +
         "</li>"
@@ -38,9 +34,7 @@ $(function () {
     delete_btn =
       '<button class="delete">削除</button> <button class="completion_button">完了</button>';
     $("#task_list").append(
-      "<li data-count=" +
-        count +
-        ">" +
+      "<li>" +
         $(this).parent().text().split("\n")[0].replace("削除 戻す", "") +
         delete_btn +
         "</li>"
